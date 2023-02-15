c815d08a216a3d396e78f0ef2e7c437ef1bb865a
Fleksy CI <developers@thingthing.co>
2023-02-15 15:39:49 +0000

    Update package to latest version

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FleksyEngine",
-            url: "https://spm.fleksy.com/FleksyEngine/FleksyEngine-3.9.4.xcframework.zip",
-            checksum: "efcbdbc3e2c9252451472039170e097f7c6fdf81c75ba55079f0bca5cf362ad6"
+            url: "https://spm.fleksy.com/FleksyEngine/FleksyEngine-3.9.5.xcframework.zip",
+            checksum: "e46c5e3e4a1b688e2d02846dc9cbc0f258df121dfc32fdb648f126ffb6111dd1"
         )
     ]
 )
