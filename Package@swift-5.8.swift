5a9f235519544efd76ea8d22c3c206e4bfc07084
Fleksy CI <developers@thingthing.co>
2023-09-15 11:20:01 +0000

    Update package to latest version (swift-tools-version 5.7)

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FleksyEngine",
-            url: "https://spm.fleksy.com/FleksyEngine/v3.12.25/FleksyEngine-Xcode14.xcframework.zip",
-            checksum: "f3319ff3d6d016309c681dfaf9c3c9080086869e0b09f2dd6919778445ccba76"
+            url: "https://spm.fleksy.com/FleksyEngine/v3.12.26/FleksyEngine-Xcode14.xcframework.zip",
+            checksum: "8721ea07121dbaae7ec83ce911202da5b0b0a61be93ace819670aeefc5030a3b"
         )
     ]
 )
